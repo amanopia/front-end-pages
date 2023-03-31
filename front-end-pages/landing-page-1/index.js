b76b06eb899b3c22129ba781502dba09cb1384c2
unknown <amanvermaspace@gmail.com>
2023-03-31 20:48:16 +0530

    completed animation for headings and navigation in the trends section

--- front-end-pages/landing-page-1/index.js
+++ front-end-pages/landing-page-1/index.js
@@ -1,7 +1,10 @@
 const navLinks = document.querySelectorAll('.nav-links');
 const brandLogo = document.querySelectorAll('.brand-logo');
 const headingChildNodes = document.querySelector('.headings').childNodes;
-console.
+const marketTrendUl = document.querySelector('.market-trend-navigation').childNodes[1].childNodes;
+const searchBox = document.querySelector('.market-trend-navigation').childNodes[3];
+console.log(searchBox)
+
 for(let i = 1; i<navLinks.length; i++){
     navLinks[0].style.fontWeight = 700;
     navLinks[i].style.color = "var(--color-disabled)";
@@ -19,17 +22,29 @@ for(let i = 1; i<navLinks.length;i++){
 }
 
 // INTERSECTION OBSERVER
-
 const observer = new IntersectionObserver(function(entries){
     entries.forEach(function(entry){
         entry.target.classList.toggle('show', entry.isIntersecting);
         if(entry.isIntersecting) observer.unobserve(entry.target);
     })
 },{
-   rootMargin: "-100px"
+   rootMargin: "-50px"
 })
 
 brandLogo.forEach(function(logo){
     observer.observe(logo)
 })
 
+// -------- MARKET TREND --------
+for(let i = 1; i<headingChildNodes.length; i+= 2){
+    observer.observe(headingChildNodes[i]);
+}
+
+let td = 0.5;
+for(let i = 1; i<marketTrendUl.length; i+= 2){
+    observer.observe(marketTrendUl[i]);
+    marketTrendUl[i].style.transitionDelay = `${td+=0.1}s`;
+}
+
+searchBox.style.transitionDelay = `${td}s`;
+observer.observe(searchBox);
